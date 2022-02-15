24f5ac56b0f2746d53a6e8df356e2e1899da7a93
Marcus <marcusmoraes2010@hotmail.com>
2022-02-15 15:00:39 -0300

    Preparando revisão

--- src/components/FormComponents/index.js
+++
@@ -1,8 +0,0 @@
-import Container from "./Container";
-import Form from "./Form";
-import Input from "./Input";
-import StyledLink from "./StyledLink";
-import Button from "./Button";
-import Loading from "./Loading";
-
-export { Container, Form, Input, StyledLink, Button, Loading };
